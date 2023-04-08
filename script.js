88b9870803776367be7b45e19028a389674de50b
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-04-08 10:51:20 -0600

    menu populated

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 import { menuArray } from "./data.js";
+
+const menu = document.getElementById("menu");
+
+function renderMenu() {
+  let menuHtml = ``;
+  menuArray.forEach((item) => {
+    menuHtml += `
+    <div class="menu-item">
+        <div class="menu-image">${item.emoji}</div>
+        <div class="menu-description">
+            <p class="menu-item-name">${item.name}</p>
+            <p class="menu-item-ingredients">${item.ingredients}</p>
+            <p class="menu-item-price">$${item.price}</p>
+        </div>
+        <button class="add-button" data-button="${item.id}">+</button>
+    </div>
+        `;
+  });
+
+  menu.innerHTML = menuHtml;
+}
+
+renderMenu();
