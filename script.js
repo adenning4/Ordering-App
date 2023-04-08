206a622b908032404963ccdadd56eece8f9ab4d3
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-04-08 12:33:29 -0600

    rendering selected items

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-import { menuArray } from "./data.js";
+import { menuArray, orderSummaryArray } from "./data.js";
 
 renderMenu();
 
+document.addEventListener("click", clickListener);
+
 function renderMenu() {
-  const menu = document.getElementById("menu");
+  const menuEl = document.getElementById("menu");
   let menuHtml = ``;
   menuArray.forEach((item) => {
     menuHtml += `
@@ -14,10 +16,51 @@ function renderMenu() {
             <p class="menu-item-ingredients">${item.ingredients}</p>
             <p class="menu-item-price">$${item.price}</p>
         </div>
-        <button class="add-button" data-button="${item.id}">+</button>
+        <button class="add-button" data-addbutton="${item.id}">+</button>
     </div>
         `;
   });
 
-  menu.innerHTML = menuHtml;
+  menuEl.innerHTML = menuHtml;
+}
+
+function clickListener(e) {
+  if (e.target.dataset.addbutton) {
+    addMenuItem(Number(e.target.dataset.addbutton));
+  }
+}
+
+function addMenuItem(buttonId) {
+  const menuItem = getMenuItem(buttonId);
+  if (!orderSummaryArray.includes(menuItem)) {
+    orderSummaryArray.push(menuItem);
+  }
+  renderOrderSummary();
+}
+
+function getMenuItem(buttonId) {
+  return menuArray.filter((item) => item.id === buttonId)[0];
+}
+
+function renderOrderSummary() {
+  if (orderSummaryArray.length) {
+    const orderSummaryDetailsEl = document.getElementById(
+      "order-summary-details"
+    );
+    let orderSummaryDetailsHtml = ``;
+    orderSummaryArray.forEach((item) => {
+      orderSummaryDetailsHtml += `
+        <div class="order-summary-item">
+            <div class="order-summary-item-name">${item.name}</div>
+            <button
+              class="order-summary-remove-item"
+              data-removebutton="${item.id}">
+              remove
+            </button>
+          </div>
+          <div class="order-summary-item-price">${item.price}</div>
+        `;
+    });
+    orderSummaryDetailsEl.innerHTML = orderSummaryDetailsHtml;
+  }
 }
